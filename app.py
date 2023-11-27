2c8d50f230c987a00132784856f29fe44b656f35
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-27 14:43:57 +0530

    <feat>: [migrate from fastAPI to flask and adds the NN]

--- app.py
+++ app.py
@@ -1,67 +1,57 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import pickle
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import tensorflow as tf
 import pandas as pd
 import numpy as np
+import math
 
-app = FastAPI()
-
-# Enable CORS for all routes
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes with default settings
 
 # Load model and encoder
-model = pickle.load(open('DentAIv1.pkl', 'rb'))
+model = tf.keras.models.load_model('DentNNv2.h5')
+
+class Item:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
 
-class Item(BaseModel):
-    Chief_complaint: float
-    Nature_of_Pain: float
-    Severity_of_pain: float
-    Onset_and_mode_of_pain: float
-    Factors_which_worsens_the_pain: float
-    Is_the_swelling_painful: float
-    Has_the_swelling_changed_since_it_was_first_noticed: float
-    Does_the_swelling_changes_during_normal_activities: float
-    Is_the_ulcer_painful: float
-    Is_there_bleeding_from_the_ulcer: float
-    Is_there_discharge_from_the_ulcer: float
-    Is_there_a_foul_smell_from_the_ulcer: float
-    Do_the_ulcers_interfere_with_daily_activities: float
-    Has_the_ulcer_changed_since_first_noticed: float
-    Have_you_had_similar_ulcers: float
-    Is_there_bleeding_in_the_gums: float
-    Is_there_pain_in_the_gums: float
-    If_any_tooth_teeth_is_are_mobile_what_is_the_degree_of_mobility: float
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        input_data = request.json
+
+        # Convert string values to numeric types
+        input_data = {key: float(value) for key, value in input_data.items()}
+
+        # Create an Item instance
+        input_item = Item(**input_data)
+
+        # Convert Item object to DataFrame
+        input_df = pd.DataFrame([input_item.__dict__])
+
+        # Fill NaN values with 0
+        input_df = input_df.fillna(0)
+
+        # Extract inputs as a numpy array
+        inputs = input_df.values
+
+        # Make predictions using the pre-trained model
+        prediction = model.predict(inputs).tolist()
+
+        # Round down the predicted value to the nearest integer
+        rounded_prediction = math.floor(prediction[0][0])
+
+        return jsonify({"prediction": rounded_prediction})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
-@app.post("/predict")
-async def predict(item: Item):
-    # Convert input data to DataFrame
-    input_data = pd.DataFrame(item.dict(), index=[0])
+@app.route('/predict', methods=['OPTIONS'])
+def handle_options():
+    return jsonify({"message": "Options"})
 
-    # Convert DataFrame columns to numeric
-    input_data = input_data.apply(pd.to_numeric, errors='coerce')
-
-    # Fill NaN values with 0
-    input_data = input_data.fillna(0)
-
-    # Make predictions using the pre-trained model
-    prediction = int(model.predict(input_data)[0])
-    pred_probability=int(model.predict_proba(input_data))
-    confidence=np.max(pred_probability)
-    # Return result
-    return {"prediction": prediction,
-            "confidence":confidence,}
-
-
-@app.options("/predict")
-async def handle_options(request: Request):
-    return {"message": "Options"}
-
+if __name__ == '__main__':
+    app.run(debug=True)
